7a1b12ba943abfcf17361ce13ccabbeeb2f21a0f
zoren <ss@configit.com>
2015-03-21 14:31:50 +0100

    added helper module for one-to-many-mappings

---
+++ Fools/OneToManyMap.fs
@@ -0,0 +1,18 @@
+﻿namespace Fools
+
+module OneToManyMap =
+  let findSet k m =
+    match Map.tryFind k m with
+    | None -> Set.empty
+    | Some s -> s
+
+  let add k v m =
+    let set = findSet k m
+    Map.add k (Set.add v set) m
+
+  let remove k v m =
+    let set = findSet k m
+    let set' = Set.remove v set
+    if Set.isEmpty set'
+    then Map.remove k m
+    else Map.add k set' m
