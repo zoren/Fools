885da2948469846ea514b09086594169a1d14be8
zoren <ss@configit.com>
2015-03-21 11:40:02 +0100

    added tests of simple rules

--- FoolsTests/Tests.fs
+++ FoolsTests/Tests.fs
@@ -8,36 +8,128 @@ open Swensen.Unquote.Assertions
 module Tests =
   let getEmptyInterpreter() = Fools.Interpreter []
 
+  let aFact = mkFact "A" []
+
   [<Test>]
   let ``a empty session has no facts``() =
     let i = getEmptyInterpreter()
-    let fact = mkFact "A" []
-    test <@ not <| i.HasFact fact @>
+    test <@ not <| i.HasFact aFact @>
     
   [<Test>]
   let ``a fact can be added``() =
     let i = getEmptyInterpreter()
-    let fact = mkFact "A" []    
-    i.Insert fact
-    test <@ i.HasFact fact @>
+    i.Insert aFact
+    test <@ i.HasFact aFact @>
 
   [<Test>]
   let ``reading fact causes exception``() =
     let i = getEmptyInterpreter()
-    let fact = mkFact "A" []    
-    i.Insert fact
-    raises<System.ArgumentException>  <@ i.Insert fact @>
+    i.Insert aFact
+    raises<System.ArgumentException>  <@ i.Insert aFact @>
 
   [<Test>]
   let ``a fact can be added and removed``() =
     let i = getEmptyInterpreter()
-    let fact = mkFact "A" []    
-    i.Insert fact
-    i.Retract fact
-    test <@ not <| i.HasFact fact @>
+    i.Insert aFact
+    i.Retract aFact
+    test <@ not <| i.HasFact aFact @>
 
   [<Test>]
   let ``retracting non-inserted fact causes exception``() =
     let i = getEmptyInterpreter()
-    let fact = mkFact "A" []    
-    raises<System.ArgumentException> <@ i.Retract fact @>
+    raises<System.ArgumentException> <@ i.Retract aFact @>
+
+  [<Test>]
+  let ``rule with no assumptions triggers``() =
+    let i = Fools.Interpreter [[], AST.Insert "A"]
+    test <@ i.HasFact aFact @>
+
+  let bFact = mkFact "B" []
+
+  let mkAImpliesB() = Fools.Interpreter [["A"], AST.Insert "B"]
+
+  [<Test>]
+  let ``rule with one assumption does not trigger, until assumption is fulfilled``() =
+    let i = mkAImpliesB()
+    test <@ not <| i.HasFact bFact @>
+
+  [<Test>]
+  let ``rule with one assumption triggers when assumption is fulfilled``() =
+    let i = mkAImpliesB()
+    i.Insert aFact
+    test <@ i.HasFact bFact @>
+
+  [<Test>]
+  let ``a fact created by a rule cannot be removed``() =
+    let i = mkAImpliesB()
+    i.Insert aFact
+    i.Retract bFact
+    test <@ i.HasFact bFact @>
+
+  [<Test>]
+  let ``a fact confirmed by a rule cannot be removed``() =
+    let i = mkAImpliesB()
+    i.Insert aFact
+    i.Insert bFact
+    i.Retract bFact
+    test <@ i.HasFact aFact @>
+    test <@ i.HasFact bFact @>
+
+  [<Test>]
+  let ``a fact confirmed by a rule will be removed when the rule deactivates``() =
+    let i = mkAImpliesB()
+    i.Insert aFact
+    i.Insert bFact
+    i.Retract bFact
+    i.Retract aFact
+    test <@ not <| i.HasFact aFact @>
+    test <@ not <| i.HasFact bFact @>
+
+  [<Test>]
+  let ``a fact is not retracted when it's no longer confirmed by the system``() =
+    let i = mkAImpliesB()
+    i.Insert aFact
+    i.Insert bFact
+    i.Retract aFact
+    test <@ not <| i.HasFact aFact @>
+    test <@ i.HasFact bFact @>
+
+  // circularity
+  [<Test>]
+  let ``a rule can confirm its own assumption``() =
+    let i = Fools.Interpreter [["A"], AST.Insert "A"]    
+    i.Insert aFact
+    test <@ i.HasFact aFact @>
+
+  [<Test>]
+  let ``a fact can be removed if it's circularily confirmed by a rule``() =
+    let i = Fools.Interpreter [["A"], AST.Insert "A"]    
+    i.Insert aFact
+    i.Retract aFact
+    test <@ i.HasFact aFact @>
+
+  [<Test>]
+  let ``circularity of two``() =
+    let i = Fools.Interpreter [["A"], AST.Insert "B"
+                               ["B"], AST.Insert "A"]    
+    i.Insert aFact
+    test <@ i.HasFact aFact @>
+    test <@ i.HasFact bFact @>
+
+  [<Test>]
+  let ``circularity of two2``() =
+    let i = Fools.Interpreter [["A"], AST.Insert "B"
+                               ["B"], AST.Insert "A"]    
+    i.Insert aFact
+    i.Retract bFact
+    test <@ i.HasFact aFact @>
+    test <@ i.HasFact bFact @>
+
+  [<Test>]
+  let ``circularity of two3``() =
+    let i = Fools.Interpreter [["A"], AST.Insert "B"
+                               ["B"], AST.Insert "A"]    
+    i.Insert aFact
+    i.Retract aFact
+    test <@ not <| i.HasFact aFact @>
+    test <@ not <| i.HasFact bFact @>
